1d0a684360fea83284290d8cefc01196b51b298a
sanny.io <3054653+sanny-io@users.noreply.github.com>
2022-04-26 00:59:00 -0700

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps as Props } from 'next/app'
+
+import '../index.css'
+import Head from 'next/head'
+import Footer from '../components/Footer'
+import MainLayout from '../components/MainLayout'
+
+export default function App({ Component, pageProps }: Props) {
+  return (
+    <>
+      <Head>
+        <title></title>
+        {/* <base href="/" target="_blank"></base> */}
+        <meta name="description" content="" />
+        <meta name="theme-color" content="" />
+      </Head>
+
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+
+      <Footer />
+    </>
+  )
+}
